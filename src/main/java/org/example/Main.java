d5cbadf67dc81743d9f57618f4119f67d4736abe
dinesh <dineshkumarm2912@gmail.com>
2026-01-26 16:49:40 +0530

    Added code for merge,quick,insertion sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {
-                1, 2, 3, 4, 5,
+        int[] original = {
                 6, 7, 8, 9, 10,
+                16, 17, 18, 19, 20,
+                1, 2, 3, 4, 5,
                 11, 12, 13, 14, 15,
-                16, 17, 18, 19, 20
         };
-        System.out.println("Largest Element in the given array is "+LargestElementProblem.findLargestElement(arr));
-        System.out.println("Second largest Element in the given array is "+LargestElementProblem.findSecondLargest(arr));
-
+        System.out.println("Largest Element in the given array is "+LargestElementProblem.findLargestElement(original));
+        System.out.println("Second largest Element in the given array is "+LargestElementProblem.findSecondLargest(original));
+        int[] arr1 = original.clone();
+        SortingAlgorithms.SelectionSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+        int[] arr2 = original.clone();
+        SortingAlgorithms.BubbleSort(arr2);
+        System.out.println(Arrays.toString(arr2));
+        int[] arr3 = original.clone();
+        System.out.println(Arrays.toString(arr3));
+        SortingAlgorithms.InsertionSort(arr3);
+        System.out.println(Arrays.toString(arr3));
+      //  int[] ans = SortingAlgorithms.merge(arr1,arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(original));
+        int[] arr4 = original.clone();
+        System.out.println(Arrays.toString( SortingAlgorithms.mergeSort(arr4,0,arr4.length-1)));
+        System.out.println(Arrays.toString(original));
+        int[] arr5 = original.clone();
+        SortingAlgorithms.quickSort(arr5,0,arr5.length-1);
+        System.out.println(Arrays.toString(arr5));
     }
+
 }
--- src/main/java/org/example/SortingAlgorithms.java
+++ src/main/java/org/example/SortingAlgorithms.java
@@ -1,4 +1,134 @@
 package org.example;
 
+import java.util.ArrayList;
+
+
 public class SortingAlgorithms {
+    public static void SelectionSort(int[] arr){
+        for(int i =0; i<arr.length;i++){
+            int smallIndex = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[smallIndex])smallIndex=j;
+            }
+            swap(arr,i,smallIndex);
+        }
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+        public static void BubbleSort(int[] arr){
+            for(int i=arr.length-1;i>=0;i--){
+                for(int j=0;j<i;j++){
+                    if(arr[j]>arr[j+1]){
+                        swap(arr,j,j+1);
+                    }
+                }
+            }
+        }
+        public static void InsertionSort(int[] arr){
+        for(int i =1;i<arr.length;i++){
+            int j =i-1;
+            while(j>=0 && arr[j]>arr[j+1]){
+                swap(arr,j,j+1);
+                j--;
+            }
+        }
+    }
+    public static int[] mergeSort(int[] arr,int i, int j){
+        if(i==j) {
+            return new int[]{arr[i]};
+        };
+        int mid = (i+j)/2;
+        int[] array1 = mergeSort(arr,i,mid);
+        int[] array2 = mergeSort(arr,mid+1,j);
+        return merge(array1,array2);
+    }
+    public static int[] merge(int[] arr,int[] arr1){
+        int p1=0,p2=0,p3=0;
+        int n = arr.length;
+        int m = arr1.length;
+        int[] ans = new int[n+m];
+        while(p1<n && p2<m){
+            if(arr[p1]<arr1[p2]) {
+                ans[p3] = arr[p1];
+                p1++;
+                p3++;
+            }else{
+                ans[p3]=arr1[p2];
+                p2++;
+                p3++;
+            }
+        }
+        while(p1<n){
+            ans[p3]=arr[p1];
+            p1++;
+            p3++;
+        }
+        while(p2<m){
+            ans[p3]=arr1[p2];
+            p2++;
+            p3++;
+        }
+        return ans;
+//
+//        public class MergeSort {
+//
+//            public static void mergeSort(int[] arr, int left, int right) {
+//                if (left >= right) return;
+//
+//                int mid = left + (right - left) / 2;
+//
+//                mergeSort(arr, left, mid);
+//                mergeSort(arr, mid + 1, right);
+//
+//                merge(arr, left, mid, right);
+//            }
+//
+//            private static void merge(int[] arr, int left, int mid, int right) {
+//                int[] temp = new int[right - left + 1];
+//
+//                int i = left, j = mid + 1, k = 0;
+//
+//                while (i <= mid && j <= right) {
+//                    if (arr[i] <= arr[j]) {
+//                        temp[k++] = arr[i++];
+//                    } else {
+//                        temp[k++] = arr[j++];
+//                    }
+//                }
+//
+//                while (i <= mid) temp[k++] = arr[i++];
+//                while (j <= right) temp[k++] = arr[j++];
+//
+//                // copy back to original array
+//                for (int x = 0; x < temp.length; x++) {
+//                    arr[left + x] = temp[x];
+//                }
+//            }
+//        }
+
+    }
+    public static void quickSort(int[] arr,int low, int high){
+        if(low>=high)return;
+        int mid = rearrange(arr,low,high);
+        quickSort(arr,low,mid-1);
+        quickSort(arr,mid+1,high);
+    }
+    public static int rearrange(int[] arr,int low, int high){
+        int pivot = arr[low];
+        int p1=low+1,p2=high;
+        while(p1<=p2){
+            if(arr[p1]<pivot)p1++;
+            else if(arr[p2]>pivot)p2--;
+            else{
+                swap(arr,p1,p2);
+                p1++;
+                p2--;
+            }
+        }
+        swap(arr,low,p2);
+        return p2;
+    }
 }
