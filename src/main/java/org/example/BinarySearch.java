06dd3338a86238edc915e49af0e776f506109b2a
dinesh <dineshkumarm2912@gmail.com>
2026-01-30 23:47:08 +0530

    Added code to find the squareRoot using findSquareRoot

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -116,4 +116,20 @@ public class BinarySearch {
         // Return peak index
         return low;
     }
+    public static int findSquareRoot(int x){
+        if(x<2)return x;
+        int left = 1; int right = x/2;
+        int ans =0;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(mid == x / mid)return mid;
+            else if(mid*mid<x) {
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
 }
