4002d4ef3de7ac7111ce8fe42bd97afbe88153fa
dinesh <dineshkumarm2912@gmail.com>
2026-02-08 21:16:39 +0530

    Added CODE for insertiona and deletion in linked list

---
+++ src/main/java/org/example/LinkedList.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class LinkedList {
+    public static Node insertionInLinkedList(Node head, int value, int position) {
+        Node newNode = new Node(value);
+
+        // Case 1: Insert at beginning
+        if (position == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        // Case 2: Empty list but position > 1
+        if (head == null) {
+            return null; // invalid position
+        }
+
+        Node temp = head;
+
+        // Traverse to node before insertion point
+        for (int place = 1; place < position - 1 && temp.next != null; place++) {
+            temp = temp.next;
+        }
+
+        // Insert node
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+        return head;
+    }
+
+
+}
