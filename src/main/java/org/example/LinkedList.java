76610c5a9aeb72a4e915efa3634f8e6e7cc90721
dinesh <dineshkumarm2912@gmail.com>
2026-02-09 23:40:28 +0530

    added code to find the cycle

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -28,6 +28,16 @@ public class LinkedList {
 
         return head;
     }
+    public boolean hasCycle(Node head) {
+        Node fast=head;
+        Node slow=head;
+        while(fast!=null && fast.next!=null ){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)return true;
+        }
+        return false;
+    }
 
 
 }
