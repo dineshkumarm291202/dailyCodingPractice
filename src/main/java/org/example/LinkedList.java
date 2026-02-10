5c030c5b86081b751a9582daf6846eccb2484873
dinesh <dineshkumarm2912@gmail.com>
2026-02-10 23:32:10 +0530

    Added code to reverse doubly linked list

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Stack;
+
 public class LinkedList {
     public static Node insertionInLinkedList(Node head, int value, int position) {
         Node newNode = new Node(value);
@@ -38,6 +40,35 @@ public class LinkedList {
         }
         return false;
     }
+    public Node reverseDLL(Node head) {
+        // If list is empty or has one element, return it
+        if (head == null || head.next == null) {
+            return head;
+        }
 
+        // Stack to store node values
+        Stack<Integer> st = new Stack<>();
+
+        // Pointer to traverse list
+        Node temp = head;
+
+        // Push all node data to stack
+        while (temp != null) {
+            st.push(temp.data);
+            temp = temp.next;
+        }
+
+        // Reset temp to head
+        temp = head;
+
+        // Replace node values from stack
+        while (temp != null) {
+            temp.data = st.pop();
+            temp = temp.next;
+        }
+
+        // Return updated head
+        return head;
+    }
 
 }
